11ab6fe38b7a175b564d7ea34b89236c07ed5c7f
eric-roland <eric.roland@gmail.com>
2012-10-08 13:04:23 -0400

    deletes and adds controller

---
+++ OhHeyWorld/OHWBaseViewController.h
@@ -0,0 +1,13 @@
+//
+//  OHWBaseViewController.h
+//  OhHeyWorld
+//
+//  Created by Eric Roland on 10/8/12.
+//  Copyright (c) 2012 Oh Hey World, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OHWBaseViewController : UIViewController
+
+@end
---
+++ OhHeyWorld/OHWBaseViewController.m
@@ -0,0 +1,38 @@
+//
+//  OHWBaseViewController.m
+//  OhHeyWorld
+//
+//  Created by Eric Roland on 10/8/12.
+//  Copyright (c) 2012 Oh Hey World, Inc. All rights reserved.
+//
+
+#import "OHWBaseViewController.h"
+
+@interface OHWBaseViewController ()
+
+@end
+
+@implementation OHWBaseViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
