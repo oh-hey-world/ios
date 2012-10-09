9b06d46450f68c865a7cb88dce7285caea7d2d5b
eric-roland <eric.roland@gmail.com>
2012-10-09 10:40:42 -0400

    adds files for reskit

--- OhHeyWorld/OHWAppDelegate.h
+++ OhHeyWorld/OHWAppDelegate.h
@@ -10,6 +10,7 @@
 #import <FacebookSDK/FacebookSDK.h>
 #import "OHWLoginViewController.h"
 #import "OHWCheckinViewController.h"
+#import <RestKit/RestKit.h>
 
 @class OHWCheckinViewController;
 @class OHWLoginViewController;
@@ -20,6 +21,13 @@
 @property (strong, nonatomic) UINavigationController *navController;
 @property (strong, nonatomic) OHWCheckinViewController *checkinViewController;
 @property (strong, nonatomic) OHWLoginViewController *loginViewController;
+@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
+@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
+@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (nonatomic, retain) NSMutableData *data;
+
+- (void)saveContext;
+- (NSString *)applicationDocumentsDirectory;
 
 - (BOOL)openSessionWithAllowLoginUI:(BOOL)allowLoginUI;
 - (void)showLoginView;
--- OhHeyWorld/OHWAppDelegate.m
+++ OhHeyWorld/OHWAppDelegate.m
@@ -13,6 +13,9 @@ NSString *const SessionStateChangedNotification = @"com.ohheyworld.OhHeyWorld:Se
 @implementation OHWAppDelegate
 
 @synthesize window = _window;
+@synthesize managedObjectContext = __managedObjectContext;
+@synthesize managedObjectModel = __managedObjectModel;
+@synthesize persistentStoreCoordinator = __persistentStoreCoordinator;
 @synthesize checkinViewController = _checkinViewController;
 @synthesize navController = _navController;
 @synthesize loginViewController = _loginViewController;
@@ -142,6 +145,114 @@ NSString *const SessionStateChangedNotification = @"com.ohheyworld.OhHeyWorld:Se
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   return YES;
+}
+
+- (void)saveContext
+{
+  NSError *error = nil;
+  NSManagedObjectContext *managedObjectContext = self.managedObjectContext;
+  if (managedObjectContext != nil)
+  {
+    if ([managedObjectContext hasChanges] && ![managedObjectContext save:&error])
+    {
+      /*
+       Replace this implementation with code to handle the error appropriately.
+       
+       abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development. If it is not possible to recover from the error, display an alert panel that instructs the user to quit the application by pressing the Home button.
+       */
+      NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
+      abort();
+    }
+  }
+}
+
+#pragma mark - Core Data stack
+
+/**
+ Returns the managed object context for the application.
+ If the context doesn't already exist, it is created and bound to the persistent store coordinator for the application.
+ */
+- (NSManagedObjectContext *)managedObjectContext
+{
+  if (__managedObjectContext != nil)
+  {
+    return __managedObjectContext;
+  }
+  
+  NSPersistentStoreCoordinator *coordinator = [self persistentStoreCoordinator];
+  if (coordinator != nil)
+  {
+    __managedObjectContext = [[NSManagedObjectContext alloc] init];
+    [__managedObjectContext setPersistentStoreCoordinator:coordinator];
+    [__managedObjectContext setRetainsRegisteredObjects:YES];
+  }
+  return __managedObjectContext;
+}
+
+/**
+ Returns the managed object model for the application.
+ If the model doesn't already exist, it is created from the application's model.
+ */
+- (NSManagedObjectModel *)managedObjectModel
+{
+  if (__managedObjectModel != nil)
+  {
+    return __managedObjectModel;
+  }
+  NSURL *modelURL = [[NSBundle mainBundle] URLForResource:@"Model" withExtension:@"momd"];
+  __managedObjectModel = [[NSManagedObjectModel alloc] initWithContentsOfURL:modelURL];
+  return __managedObjectModel;
+}
+
+/**
+ Returns the persistent store coordinator for the application.
+ If the coordinator doesn't already exist, it is created and the application's store added to it.
+ */
+- (NSPersistentStoreCoordinator *)persistentStoreCoordinator
+{
+  if (__persistentStoreCoordinator != nil)
+  {
+    return __persistentStoreCoordinator;
+  }
+  
+	NSString *storePath = [[self applicationDocumentsDirectory] stringByAppendingPathComponent: @"app.sqlite"];
+	/*
+	 Set up the store.
+	 For the sake of illustration, provide a pre-populated default store.
+	 */
+	NSFileManager *fileManager = [NSFileManager defaultManager];
+	// If the expected store doesn't exist, copy the default store.
+	if (![fileManager fileExistsAtPath:storePath]) {
+		NSString *defaultStorePath = [[NSBundle mainBundle] pathForResource:@"App" ofType:@"sqlite"];
+		if (defaultStorePath) {
+			[fileManager copyItemAtPath:defaultStorePath toPath:storePath error:NULL];
+		}
+	}
+	NSURL *storeUrl = [NSURL fileURLWithPath:storePath];
+	
+	NSDictionary *options = [NSDictionary dictionaryWithObjectsAndKeys:[NSNumber numberWithBool:YES], NSMigratePersistentStoresAutomaticallyOption, [NSNumber numberWithBool:YES], NSInferMappingModelAutomaticallyOption, nil];
+  __persistentStoreCoordinator = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel: [self managedObjectModel]];
+  
+	NSError *error;
+	if (![__persistentStoreCoordinator addPersistentStoreWithType:NSSQLiteStoreType configuration:nil URL:storeUrl options:options error:&error]) {
+		// Update to handle the error appropriately.
+		NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
+		exit(-1);  // Fail
+  }
+  
+  return __persistentStoreCoordinator;
+}
+
+#pragma mark - Application's Documents directory
+
+/**
+ Returns the path to the application's documents directory.
+ */
+- (NSString *)applicationDocumentsDirectory {
+	
+  NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+  NSString *basePath = ([paths count] > 0) ? [paths objectAtIndex:0] : nil;
+  return basePath;
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application
