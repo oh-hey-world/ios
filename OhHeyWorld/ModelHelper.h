a5c90d4bbe4337d694fdd480f3a131790a18eef1
eric-roland <eric.roland@gmail.com>
2013-05-08 08:57:56 -0400

    update deletion

---
+++ OhHeyWorld/ModelHelper.h
@@ -0,0 +1,28 @@
+//
+//  ModelHelper.h
+//  OhHeyWorld
+//
+//  Created by Eric Roland on 10/10/12.
+//  Copyright (c) 2012 Oh Hey World, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "OHWAppDelegate.h"
+
+@interface ModelHelper : NSObject
+
++ (void)deleteObject:(NSManagedObject*)object;
++ (User*)getFacebookUser:(NSDictionary<FBGraphUser>*)fbUser;
++ (User*)getUserByEmail:(NSString*)email;
++ (UserProvider*)getUserProvider:(User*)user:(NSString*)providerName;
++ (UserProvider*)getOrSaveUserProvider:(NSDictionary<FBGraphUser>*)fbUser:(User*)user:(NSString*)providerName;
++ (UserLocation*)getLastUserLocation:(User*)user;
++ (NSArray*)getUserProviderFriends:(User*)user;
++ (NSArray*)getUserLocations:(User*)user;
++ (UserFriend*)getUserFriend:(User*)user:(User*)friend;
++ (UserAsset*)getDefaultUserAsset:(User*)user;
++ (void)setOldAssetDefaultsFalse:(User*)user;
++ (NSArray*)getLanguagesByIds:(NSArray*)languageIds;
++ (BOOL)isSameUser:(User*)user:(User*)friend;
+
+@end
